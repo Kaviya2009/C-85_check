2912116177d45017378243791d1123766ab9e612
Kaviya2009 <103879663+Kaviya2009@users.noreply.github.com>
2022-08-21 17:07:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
+nasa_mars_images_array = ["nasa_image_1.jpg","nasa_image_2.jpeg", "nasa_image_3.jpg","nasa_image_4.jpg"]; 
+random_number = Math.floor(Math.random() * 4); console.log(random_number);
+
 rover_width = 100;
 rover_height = 90;
 
+
+background_image = nasa_mars_images_array[random_number]; 
+console.log("background_image = " + background_image);
+rover_image = "rover.png";
+
 rover_x = 10;
 rover_y = 10;
-
-background_image = "mars.jpg";
-rover_image = "rover.png";
 
 function add()
 {
@@ -67,7 +72,7 @@ function up()
 {
     if(rover_y>=0)
     {
-        rover_y = rover_y - 10;
+        rover_y= rover_y - 10;
         console.log("When up arrow is pressed, x = " + rover_x +  " | y = " + rover_y);
         uploadBackground();
         uploadrover();
@@ -78,7 +83,7 @@ function down()
 {
     if(rover_y<=500)
     {
-        rover_y = rover_y + 10;
+        rover_y= rover_y + 10;
         console.log("When down arrow is pressed, x = " + rover_x +  " | y = " + rover_y);
         uploadBackground();
         uploadrover();
@@ -89,7 +94,7 @@ function left()
 {
     if(rover_x>=0)
     {
-        rover_x = rover_x - 10;
+        rover_x= rover_x - 10;
         console.log("When left arrow is pressed, x = " + rover_x +  " | y = " + rover_y);
         uploadBackground();
         uploadrover();
@@ -100,7 +105,7 @@ function right()
 {
     if(rover_x<=700)
     {
-        rover_y = rover_y + 10;
+        rover_y= rover_y + 10;
         console.log("When right arrow is pressed, x = " + rover_x +  " | y = " + rover_y);
         uploadBackground();
         uploadrover();
